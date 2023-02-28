43f2d2daabac73bf8e1455ef377d63424f0003b6
Enzo Caviglia <enzo.romani@unidavi.edu.br>
2023-02-28 00:25:44 +0000

    meu primeiro app

---
+++ unidade1.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+import altair as alt
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+@st.cache_data
+def load_database():
+        return pd.read_csv('brasil_estados.csv')
+
+st.title('Meu Primiero App - GCI')
+
+estados = load_database()
+
+dados, estatistica = st.tabs(['Dados','Estatistica Descritiva'])
+
+with dados:
+    regiao = st.selectbox(
+        'Selecione a Região:',
+        estados['regiao_nome'].unique()
+    )
+    st.dataframe(estados[estados['regiao_nome'] == regiao])
+    with estatistica:
+        variavel = st.selectbox(
+            'Selecione a Variavel',
+            ['area', 'populacao', 'idh', 'matricula' ]
+        )
+        st.table(estados[variavel].describe())
