8e9014ce6ac087723bd6faa824971d1c777737b5
Enzo Caviglia <enzo.romani@unidavi.edu.br>
2023-04-16 14:21:53 +0000

    att

--- unidade1.py
+++ unidade1.py
@@ -9,10 +9,9 @@ import plotly.express as px
 
 st.set_page_config(layout="wide")
 
-@st.cache_data
+@st.cache_resource
 def load_database():
-    return pd.read_csv('brasil_estados.csv'), \
-        json.load(open('brazil-states.geojson.txt'))
+    return pd.read_excel('BRCidadesRegiao.xlsx').query('estado in ["DF", "BA", "ES"]')
 
 def highlight_class_lc(s):
     if s.outlier_max == 1:
@@ -26,70 +25,41 @@ def highlight_class_lc(s):
 
 st.title('Meu primeiro App - GCI')
 
-estados, fronteiras = load_database()
+cidades = load_database()
 
-dados, estatistica, outlier, zvalues = st.tabs(['Dados', 'Estatística Descritiva', 'Outliers', 'Valores Padronizados'])
+dados, estatistica, outlier, zvalues, quadrante = st.tabs(['Dados', 'Estatística Descritiva', 'Outliers', 'Valores Padronizados', 'Quadrante'])
 
-variaveis = ['area', 'populacao', 'densidade', 'matricula', 'idh', 'receitas', 'despesas', 'rendimento', 'veiculos']
+variaveis = ['area_territorial', 'populacao_estimada', 'densidade_demografica', 'pib_per_capita', 'idhm','carros']
 
 with dados:
-    if st.checkbox('Região'):
-        regiao = st.selectbox('Selecione a Região:', estados['regiao_nome'].unique())
-        st.dataframe(estados[estados['regiao_nome'] == regiao])
+    if st.checkbox('Estado'):
+        estado = st.selectbox('Selecione o Estado:', cidades['estado'].unique())
+        st.dataframe(cidades[cidades['estado'] == estado])
     else:
-        st.table(estados)
-    with st.expander('Mapa'):
-        variavel = st.selectbox('Variável:', variaveis)
-        minimo = estados[variavel].min()
-        maximo = estados[variavel].max()
-        mapa_px = px.choropleth_mapbox(
-            data_frame = estados, 
-            geojson = fronteiras, 
-            locations='sigla', 
-            featureidkey='properties.sigla',
-            color=variavel,
-            color_continuous_scale= 'reds',
-            hover_name = 'sigla', 
-            hover_data =['uf', variavel, 'regiao_nome'],    
-            range_color=(minimo, maximo),
-            mapbox_style='carto-positron',
-            zoom=3.5, 
-            center = {"lat": -15.76, "lon": -47.88},
-            opacity=1,
-            labels={'sigla' : 'Sigla',
-                    'uf': 'Estado',
-                    'regiao_nome': 'Região'
-            },
-            width = 1200,
-            height = 800,
-            title = 'Mapa do Brasil'
-        )
-        mapa_px.update_layout(margin={'r':0,'t':0,'l':0, 'b':0})
-        mapa_px.update_traces(marker_line_width=1)
-        st.plotly_chart(mapa_px)
+        st.table(cidades)
 
 with estatistica:
     variavel = st.selectbox('Selecione a variavel', variaveis)
     col1, col2, col3, col4 = st.columns([3,1,2,1])
-    col1.altair_chart(alt.Chart(estados).mark_bar().encode(x="uf:O", y=variavel+':Q').properties(height=500))
-    col2.dataframe(round(estados[variavel].describe(),2))
-    base = alt.Chart(estados)
+    col1.altair_chart(alt.Chart(cidades).mark_bar().encode(x="municipio:O", y=variavel+':Q').properties(height=500))
+    col2.dataframe(round(cidades[variavel].describe(),2))
+    base = alt.Chart(cidades)
     bar = base.mark_bar().encode(x=alt.X(variavel+':Q', bin=True), y='count()')
     rule = base.mark_rule(color='red').encode(x='mean('+variavel+'):Q', size=alt.value(5))
     rule2 = base.mark_rule(color='green').encode(x='median('+variavel+'):Q', size=alt.value(5))
     col3.altair_chart(bar + rule + rule2)
-    col4.altair_chart(alt.Chart(estados).mark_boxplot().encode(y=variavel+':Q').properties(width=200))
+    col4.altair_chart(alt.Chart(cidades).mark_boxplot().encode(y=variavel+':Q').properties(width=200))
 with outlier:
     variavel = st.selectbox('Selecione a variavel para outliers', variaveis)
-    estados_var = estados[['uf', variavel]].copy()
-    iqr = estados_var[variavel].quantile(0.75) - estados_var[variavel].quantile(0.25)
-    out_min = estados_var[variavel].quantile(0.25) - (1.5 * iqr)
-    out_max = estados_var[variavel].quantile(0.75) + (1.5 * iqr)
-    limite_inferior = estados_var[variavel].mean() - (1.96 * estados_var[variavel].std() / np.sqrt(len(estados_var)))
-    limite_superior = estados_var[variavel].mean() + (1.96 * estados_var[variavel].std() / np.sqrt(len(estados_var)))
-    estados_var['outlier_min'] = estados_var[variavel].apply(lambda x : 1 if x < out_min else 0)
-    estados_var['outlier_max'] = estados_var[variavel].apply(lambda x : 1 if x > out_max else 0)
-    estados_var['class_lc'] = estados_var[variavel].apply(
+    cidades_var = cidades[['municipio', variavel]].copy()
+    iqr = cidades_var[variavel].quantile(0.75) - cidades_var[variavel].quantile(0.25)
+    out_min = cidades_var[variavel].quantile(0.25) - (1.5 * iqr)
+    out_max = cidades_var[variavel].quantile(0.75) + (1.5 * iqr)
+    limite_inferior = cidades_var[variavel].mean() - (1.96 * cidades_var[variavel].std() / np.sqrt(len(cidades_var)))
+    limite_superior = cidades_var[variavel].mean() + (1.96 * cidades_var[variavel].std() / np.sqrt(len(cidades_var)))
+    cidades_var['outlier_min'] = cidades_var[variavel].apply(lambda x : 1 if x < out_min else 0)
+    cidades_var['outlier_max'] = cidades_var[variavel].apply(lambda x : 1 if x > out_max else 0)
+    cidades_var['class_lc'] = cidades_var[variavel].apply(
         lambda x : 'abaixo' 
         if x < limite_inferior 
         else (
@@ -98,24 +68,24 @@ with outlier:
             else 'media'
         ) 
     )
-    st.dataframe(estados_var.style.apply(highlight_class_lc, axis=1))
+    st.dataframe(cidades_var.style.apply(highlight_class_lc, axis=1))
     with st.expander('Média - Intervalo de Confiança'):
         col1, col2, col3 = st.columns(3)
-        col1.metric('Média', round(estados_var[variavel].mean(),2), "0")
-        col2.metric('Limite Inferior', round(limite_inferior,2), round(limite_inferior - estados_var[variavel].mean(),2))
-        col3.metric('Limite Superior', round(limite_superior,2), round(limite_superior - estados_var[variavel].mean(),2))
-        st.altair_chart(alt.Chart(estados_var).mark_bar().encode(x="uf:O", y=variavel+':Q', color='class_lc:N').properties(height=400))        
+        col1.metric('Média', round(cidades_var[variavel].mean(),2), "0")
+        col2.metric('Limite Inferior', round(limite_inferior,2), round(limite_inferior - cidades_var[variavel].mean(),2))
+        col3.metric('Limite Superior', round(limite_superior,2), round(limite_superior - cidades_var[variavel].mean(),2))
+        st.altair_chart(alt.Chart(cidades_var).mark_bar().encode(x="municipio:O", y=variavel+':Q', color='class_lc:N').properties(height=400))        
     with st.expander('Outlier - Amplitude'):
         col1, col2, col3 = st.columns(3)
-        col1.metric('Média', round(estados_var[variavel].mean(),2), "0")
-        col2.metric('Limite Inferior', round(out_min,2), round(out_min - estados_var[variavel].mean(),2))
-        col3.metric('Limite Superiorr', round(out_max,2), round(out_max - estados_var[variavel].mean(),2))
-        st.altair_chart(alt.Chart(estados_var).mark_bar().encode(x="uf:O", y=variavel+':Q', color='outlier_max:N').properties(height=400))
+        col1.metric('Média', round(cidades_var[variavel].mean(),2), "0")
+        col2.metric('Limite Inferior', round(out_min,2), round(out_min - cidades_var[variavel].mean(),2))
+        col3.metric('Limite Superiorr', round(out_max,2), round(out_max - cidades_var[variavel].mean(),2))
+        st.altair_chart(alt.Chart(cidades_var).mark_bar().encode(x="municipio:O", y=variavel+':Q', color='outlier_max:N').properties(height=400))
     with st.expander('Sem Outlier - Nova Média'):
-        estados_var_out = estados_var[(estados_var['outlier_max'] == 0) & (estados_var['outlier_min'] == 0)].copy()
-        limite_inferior = estados_var_out[variavel].mean() - (1.96 * estados_var_out[variavel].std() / np.sqrt(len(estados_var_out)))
-        limite_superior = estados_var_out[variavel].mean() + (1.96 * estados_var_out[variavel].std() / np.sqrt(len(estados_var_out)))
-        estados_var_out['class_lc'] = estados_var_out[variavel].apply(
+        cidades_var_out = cidades_var[(cidades_var['outlier_max'] == 0) & (cidades_var['outlier_min'] == 0)].copy()
+        limite_inferior = cidades_var_out[variavel].mean() - (1.96 * cidades_var_out[variavel].std() / np.sqrt(len(cidades_var_out)))
+        limite_superior = cidades_var_out[variavel].mean() + (1.96 * cidades_var_out[variavel].std() / np.sqrt(len(cidades_var_out)))
+        cidades_var_out['class_lc'] = cidades_var_out[variavel].apply(
             lambda x : 'abaixo' 
             if x < limite_inferior 
             else (
@@ -125,43 +95,43 @@ with outlier:
             ) 
         )
         col1, col2, col3 = st.columns(3)
-        col1.metric('Média', round(estados_var_out[variavel].mean(),2), "0")
-        col2.metric('Limite Inferior', round(limite_inferior,2), round(limite_inferior - estados_var_out[variavel].mean(),2))
-        col3.metric('Limite Superiorr', round(limite_superior,2), round(limite_superior - estados_var_out[variavel].mean(),2))
-        st.altair_chart(alt.Chart(estados_var_out).mark_bar().encode(x="uf:O", y=variavel+':Q', color='class_lc:N').properties(height=400))        
+        col1.metric('Média', round(cidades_var_out[variavel].mean(),2), "0")
+        col2.metric('Limite Inferior', round(limite_inferior,2), round(limite_inferior - cidades_var_out[variavel].mean(),2))
+        col3.metric('Limite Superiorr', round(limite_superior,2), round(limite_superior - cidades_var_out[variavel].mean(),2))
+        st.altair_chart(alt.Chart(cidades_var_out).mark_bar().encode(x="municipio:O", y=variavel+':Q', color='class_lc:N').properties(height=400))        
 with zvalues:
     colunas = st.multiselect('colunas', variaveis)
     if len(colunas) > 0:
         sel = colunas
-        sel.insert(0, "uf")
-        estadosz = estados[sel].copy()
+        sel.insert(0, "municipio")
+        cidadesz = cidades[sel].copy()
         listaz = []
-        for col in estadosz.columns:
-            if col != 'uf':
-                media = estadosz[col].mean()
-                dp = estadosz[col].std()
-                estadosz['z_'+col] = estadosz[col].apply(lambda x : (x - media) / dp)
+        for col in cidadesz.columns:
+            if col != 'municipio':
+                media = cidadesz[col].mean()
+                dp = cidadesz[col].std()
+                cidadesz['z_'+col] = cidadesz[col].apply(lambda x : (x - media) / dp)
                 listaz.append('z_'+col)
-        listaz.insert(0, "uf")
+        listaz.insert(0, "municipio")
         with st.expander('Dados'):
-            st.dataframe(estadosz.style.hide_index().background_gradient(cmap='Blues'))
+            st.dataframe(cidadesz.style.hide_index().background_gradient(cmap='Blues'))
         with st.expander('Gráfico'):
             graphz = pd.DataFrame()
             for zvalue in listaz:
-                if zvalue != 'uf':
-                    for index, row in estadosz.iterrows():
-                        graphz = graphz.append({'uf': row['uf'], 'variable': zvalue, 'valor': row[zvalue]}, ignore_index=True)
-            st.altair_chart(alt.Chart(graphz).mark_bar(opacity=0.5).encode(x='uf:O', y='valor:Q', color='variable:N').properties(height=400))    
+                if zvalue != 'municipio':
+                    for index, row in cidadesz.iterrows():
+                        graphz = graphz.append({'municipio': row['municipio'], 'variable': zvalue, 'valor': row[zvalue]}, ignore_index=True)
+            st.altair_chart(alt.Chart(graphz).mark_bar(opacity=0.5).encode(x='municipio:O', y='valor:Q', color='variable:N').properties(height=400))    
         with st.expander('Ranking'):
             if len(colunas) > 0:
-                data = estados[colunas]
+                data = cidades[colunas]
                 print(data)
                 dataz = pd.DataFrame()
                 for col in data.columns:
-                    if col != 'uf':
-                        media = estados[col].mean()
-                        dp = estados[col].std()
-                        dataz[col] = estados[col].apply(lambda x: (x - media) / dp)
+                    if col != 'municipio':
+                        media = cidades[col].mean()
+                        dp = cidades[col].std()
+                        dataz[col] = cidades[col].apply(lambda x: (x - media) / dp)
                 dataz['total'] = dataz.sum(
                     axis=1,
                     skipna=True
@@ -195,7 +165,7 @@ with zvalues:
                         else 'media'
                     )
                 )
-                datac = estados[['regiao_nome','uf']].copy()
+                datac = cidades[['municipio','estado']].copy()
                 datac = datac.merge(dataz, left_index=True, right_index=True)
                 data_sort = datac.sort_values(by='ranking')
                 st.table(data_sort.style.hide_index().background_gradient(cmap='Blues'))
